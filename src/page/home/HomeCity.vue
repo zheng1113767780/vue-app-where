ab42d39d11b410b3c99bedbca096165c7f24453d
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-07-04 11:23:28 +0800

    图片区域页面布局

--- src/page/home/HomeCity.vue
+++ src/page/home/HomeCity.vue
@@ -2,14 +2,16 @@
     <div>
         <city-header></city-header>
         <home-swiper></home-swiper>
+        <home-icons></home-icons>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/CityHeader.vue'
+import HomeIcons from './components/HomeIcons.vue'
 import HomeSwiper from './components/HomeSwiper.vue'
     export default {
-  components: { CityHeader, HomeSwiper },
+  components: { CityHeader, HomeSwiper, HomeIcons },
         name:"HomeCity"
     }
 </script>
