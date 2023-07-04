1a11574bf8e30aaa0eda41cec1369cae8452132f
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-07-04 10:31:07 +0800

    首页焦点轮播图

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
-
+Vue.use(VueAwesomeSwiper);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
--- src/page/home/HomeCity.vue
+++ src/page/home/HomeCity.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
         <city-header></city-header>
+        <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/CityHeader.vue'
+import HomeSwiper from './components/HomeSwiper.vue'
     export default {
-  components: { CityHeader },
+  components: { CityHeader, HomeSwiper },
         name:"HomeCity"
     }
 </script>
