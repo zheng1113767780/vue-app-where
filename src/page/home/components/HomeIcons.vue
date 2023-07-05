f6181b1a89eaf75e0bcd16650f01ddbd744c47c7
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-07-05 22:01:10 +0800

    图标区域逻辑实现

--- src/page/home/components/HomeIcons.vue
+++ src/page/home/components/HomeIcons.vue
@@ -1,66 +1,87 @@
 <template>
     <div class="icons">
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
-            </div>
-            <p class="icon-desc">景点门票</p>
-        </div>
+        <swiper>
+            <swiper-slide v-for="(page, index) in pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl">
+                    </div>
+                    <p class="icon-desc">{{ item.desc }}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 
 <script>
 export default {
-    name: "HomeIcons"
+    name: "HomeIcons",
+    data() {
+        return {
+            iconList: [
+                {
+                    id: '0001',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+                    desc: '景点门票'
+                },
+                {
+                    id: '0002',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+                    desc: '滑雪季'
+                },
+                {
+                    id: '0003',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+                    desc: '泡温泉'
+                },
+                {
+                    id: '0005',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+                    desc: '景点门票'
+                },
+                {
+                    id: '0006',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+                    desc: '滑雪季'
+                },
+                {
+                    id: '0007',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+                    desc: '泡温泉'
+                },
+                {
+                    id: '0008',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+                    desc: '动植园'
+                },
+                {
+                    id: '0009',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png',
+                    desc: '游乐园'
+                },
+            ]
+        }
+    },
+    computed: {
+        pages() {
+            const pages = [];
+            this.iconList.forEach((item, index) => {
+                const page = Math.floor(index / 8);
+                if (!pages[page]) {
+                    pages[page] = [];
+                }
+                pages[page].push(item);
+            })
+            return pages;
+        }
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '@/assets/styles/varibles.styl'
-.icons
- overflow: hidden;
+@import '@/assets/styles/mixins.styl'
+.icons >>> .swiper-container
  height: 0;
  padding-bottom: 50%;
  .icon
@@ -91,4 +112,5 @@ export default {
     line-height: .44rem
     text-align: center
     color: $darkTextColor
+    ellipsis();
 </style>
