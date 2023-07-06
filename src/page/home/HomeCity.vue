53e16de2c78ddbf5e3a3426210360fcb3504ea6a
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-07-06 09:16:26 +0800

    热销推荐组件开发和周末游组件开发

--- src/page/home/HomeCity.vue
+++ src/page/home/HomeCity.vue
@@ -3,15 +3,19 @@
         <city-header></city-header>
         <home-swiper></home-swiper>
         <home-icons></home-icons>
+        <home-recommend></home-recommend>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/CityHeader.vue'
 import HomeIcons from './components/HomeIcons.vue'
+import HomeRecommend from './components/HomeRecommend.vue'
 import HomeSwiper from './components/HomeSwiper.vue'
+import HomeWeekend from './components/HomeWeekend.vue'
     export default {
-  components: { CityHeader, HomeSwiper, HomeIcons },
+  components: { CityHeader, HomeSwiper, HomeIcons, HomeRecommend, HomeWeekend },
         name:"HomeCity"
     }
 </script>
