cca99c233bffd45a0c338839f362d2227840fcae
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-07-10 16:38:22 +0800

    实现header渐隐渐现效果

--- src/page/detail/DetailHome.vue
+++ src/page/detail/DetailHome.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBanner from "./components/DetailBanner.vue"
+import DetailHeader from './components/DetailHeader.vue'
 
 export default {
-    components: { DetailBanner },
+    components: { DetailBanner, DetailHeader },
     name: "DetailHome"
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="stylus" scoped>
+.content
+ height: 50rem;
+</style>
